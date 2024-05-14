2604ebeda3e9286e6ad8b6e224cbc1dae2ecb089
Anuragpandey768 <121385681+Anuragpandey768@users.noreply.github.com>
2024-05-15 00:05:51 +0530

    change

--- component/Header.tsx
+++ component/Header.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 import "../app/globals.css"
 function Header({ font }: { font?: string }) {
   return (
-    <header className='py-2 bg-dark-500'>
+    <header className='py-2 bg-gray-952'>
       <div className="max-w-[100rem] px-12 mx-auto flex justify-between">
         <Link href={'/'}>
           <h1 className='uppercase text-yellow-500 text-center py-2'>
             Easy sEll
           </h1>
         </Link>
-        <Link href={"/product/upload"} className='uppercase text-green-500 text-xl py-2 hover:text-yellow-700'>
+        <Link href={"/product/upload"} className='uppercase text-green-600 text-xl py-2 hover:text-yellow-700'>
           Upload
         </Link>
       </div>
